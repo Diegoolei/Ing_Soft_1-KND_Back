acbacb827c3a167c2c0e294201c3eda1c7753013
shirosweets <60824319+shirosweets@users.noreply.github.com>
2020-10-24 15:26:59 -0300

    Update models.py

--- models.py
+++ models.py
@@ -11,6 +11,14 @@ from pydantic import BaseModel, EmailStr
 
 
 #boards's models
+class Board(BaseModel):
+    is_fenix: bool = false
+    proclamation_promulged_fenix: int = 0
+    proclamation_promulged_mortifagos: int = 0
+    # 17 Cards = 17 bits for "binarie order"
+    # https://www.datacamp.com/community/tutorials/python-data-type-conversion
+    # 0bits a 17bits => 0 = fenix, 1 = mortifago and -1 = ¿not card?
+    deck_codification: list(int) = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 
 #histories's models
