080ef997981cda3476feb38d9284343375a14d64
shirosweets <60824319+shirosweets@users.noreply.github.com>
2020-10-24 23:47:12 -0300

    Update db_entities_relations.py
    - Add Player entity (of Lobby and Game)
    - Add Board entity
    - Add End Match

--- db_entities_relations.py
+++ db_entities_relations.py
@@ -12,11 +12,28 @@ db = Database()
 
 
 #player entity
-
+class Player(db.entity):
+    player_lobby            = Required(Lobby)
+    #player_id              = Required(User) # Depends on User
+    player_number           = Required(int, unique = True)    # Definied order
+    player_nick             = Required(str)    # = userName Depends on User
+    player_role             = Required(bool)
+    player_is_alive         = Required(bool)    # = True
+    player_chat_blocked     = Required(bool)    # = False
+    player_director         = Required(bool)
+    player_minister         = Required(bool)
 
 #board entity
+class Board(db.entity):
+    proclamation_promulged_fenix: Required(int)    # = 0
+    proclamation_promulged_death_eater: Required(int)    # = 0
+    deck_codification: Required(int)    # binarie
+    is_spell_active: Required(bool)    # = False
 
-
+#end match
+class End_Match():
+    is_end : Required(bool) # = True Depends on "Game"     
+      
 #history entity
 
 
