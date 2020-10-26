71b26ff1d4b6651a3c7e6dd39996fe1683e5a7dd
Diegoolei <71725436+Diegoolei@users.noreply.github.com>
2020-10-26 15:18:39 -0300

    Update db_entities_relations.py

--- db_entities_relations.py
+++ db_entities_relations.py
@@ -1,36 +1,38 @@
 from pony.orm import Database, PrimaryKey, Required, Optional, Set
+from datetime import datetime
 
 db = Database()
 
 # user entity
 class User(db.Entity):
-    user_id = PrimaryKey(int, auto=True) 
-    email = Required(str, unique=True)
-    username = Required(str, unique=True, max_len=16)
-    password = Required(str, max_len= 32)
-    photo = Optional(str)
-
+    user_id                 = PrimaryKey(int, auto=True) 
+    user_email              = Required(str, unique=True)
+    user_name               = Required(str, unique=True, max_len=16)
+    user_password           = Required(str, max_len= 32)
+    user_photo              = Optional(str)
+    user_create_dt          = datetime
+    user_lobby              = Set('Lobby')        # many to many relation with User-Lobby, we use '' because Player is declarated after this call
+    user_player             = Set('Player')      # one to many relation with User-Player, we use '' because Player is declarated after this call
     
 # lobby entity
 
 class Lobby(db.Entity):
-    match_id            = PrimaryKey(int, auto = True)
-    lobby_creator       = Required(str)     # User.username
-    lobby_name          = Required(str, unique=True)
-    max_amount_players  = Required(int)
-    min_amount_players  = Required(int)
-    match_started       = Required(bool)
-    players             = Set('Player')     # 1-* relation with Lobby-Player, we use '' because Player is declarated after this call 
- 
+    lobby_id                = PrimaryKey(int, auto = True)
+    lobby_creator           = Set(User)         # many to many relation with Lobby-User, we use '' because Player is declarated after this call
+    lobby_players           = Set('Player')     # one to many relation with Lobby-Player, we use '' because Player is declarated after this call
+    lobby_name              = Required(str, unique=True)
+    lobby_max_players       = Required(int)
+    lobby_min_players       = Required(int)
+    
 #game entity
 
 
 # player entity
 
 class Player(db.Entity):
-    #player_game             = Required(Game)  #Depends on Game
-    player_lobby            = Required(Lobby)
-    player_id               = Required(User) #Depends on User
+    #player_game             = Required(Game)   # one to many relation with Player-Game
+    player_lobby            = Required(Lobby)   # one to many relation with Player-Game
+    player_id               = Required(User)    # one to many relation with Player-Game
     player_number           = Required(int, unique = True)
     player_nick             = Required(str) 
     player_role             = Required(int)
